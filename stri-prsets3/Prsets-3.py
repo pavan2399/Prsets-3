4c282fe6ba108d6d0fe6751cbbf8ee4f0d085b9f
Pavan <chitlapavan23@gmail.com>
2023-01-16 20:57:56 +0530

    Add Prsets-3.py Solution

---
+++ stri-prsets3/Prsets-3.py
@@ -0,0 +1,9 @@
+"""
+Write a python script that captures USN
+of a student from std. input and converts
+all lowercase letters to
+uppercase and prints the same. """
+
+usn = input("Please enter USN\n")
+usn_upper = usn.upper()
+print(usn_upper)
